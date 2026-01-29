984b3ab022a7403cc777b7b0c6e56c3dab7630f6
Scottie Housholder (library) <scottie_housholder@taylor.edu>
2026-01-29 12:47:16 -0500

    Moved code from non-git file

--- minesweeper.py
+++ minesweeper.py
@@ -1,17 +1,29 @@
 import random
 
+def print_board(board):
+    for col in board:
+        print(col)
+
+
 def make_board(size):
     # print(list([list([0]*size)]*size)) #I think this version is slower
-    return [[0 for _ in range(size)] for _ in range(size)]
+    board = [["E" for _ in range(size)] for _ in range(size)]
+    return board
 
 
 def fill_board(board, num_mines):
-    pass
+    # print(board)
+    size = len(board)
+    for _ in range(num_mines):
+        row = random.randint(0, size-1)
+        col = random.randint(0, size-1)
+        board[row][col] = "M"
 
 
 def main():
     board = make_board(8)
-    fill_board(board)
+    fill_board(board, 10)
+    print_board(board)
 
 
 
